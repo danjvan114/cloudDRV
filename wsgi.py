1528971b81b9b9cb3b344ffaf4c9009172b6bb56
danjvan114 <danjvan114@github.com>
2026-08-08 22:19:44 +0800

    8850

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
         serve(app, host='0.0.0.0', port=8897, threads=16)
     except ImportError:
         print("Running with Flask development server...")
-        app.run(host='0.0.0.0', port=8897)
+        app.run(host='0.0.0.0', port=8850)
