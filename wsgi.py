00d4301c58a25c67b4c0df5c95531b708a91b9e5
danjvan114 <danjvan114@github.com>
2026-08-09 12:22:45 +0800

    威威

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     try:
         from waitress import serve
         print("Running with Waitress production WSGI server...")
-        serve(app, host='0.0.0.0', port=8897, threads=16)
+        serve(app, host='0.0.0.0', port=8850, threads=16)
     except ImportError:
         print("Running with Flask development server...")
         app.run(host='0.0.0.0', port=8850)
